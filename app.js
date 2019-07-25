b77fcb414b11d86c4ae5461f7e3fa74b718643fb
yar-petrov <rammzes-95@mail.ru>
2019-07-26 00:26:56 +0300

    Test

---
+++ app.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['block1', 'block2', 'block3'],
+        menu: '#menu',
+        css3: true,
+        scrollingSpeed: 1000
+    });
+});
